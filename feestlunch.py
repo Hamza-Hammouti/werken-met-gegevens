0b59ce420f48b88db01dd888558b19efd8c7c6eb
HamzaHammouti <hamzahhammouti@gmail.com>
2021-09-08 10:10:13 +0200

    Tekst bij de berekening

--- feestlunch.py
+++ feestlunch.py
@@ -2,4 +2,10 @@ croissant = 0.39
 stokbrood = 2.78
 kortingsbon = 0.50
 
-print(croissant * 17 + stokbrood * 2 - kortingsbon * 3)
+croissantaantal = 17
+stokbroodaantal = 2
+kortingsbonaantal = 3
+
+totaalprijs = croissant * croissantaantal + stokbrood * stokbroodaantal - kortingsbon * kortingsbonaantal
+
+print("De feestlunch kost je bij de bakker " + str(totaalprijs) + " euro voor de " + str(croissantaantal) +" croissantjes en de " + str(stokbroodaantal) + " stokbroden als de " + str(kortingsbonaantal) + " kortingsbonnen nog geldig zijn!")
--- speelhaldag.py
+++ speelhaldag.py
@@ -1,4 +1,12 @@
 toegangsticket = 7.45
-gameseat = 0.37                 # De prijs van de gameseat is voor 5 minuten.
+gameseat = 0.074                 # De prijs van de gameseat is voor 1 minuut.
+totaalaantalminuten = 45
 
-print(toegangsticket * 3 + gameseat * 27)
+toegangsticketaantal = 3
+gameseataantal = 27
+
+totaalprijs = toegangsticket * toegangsticketaantal + ((totaalaantalminuten * gameseat) * 3)
+
+print(totaalprijs)
+
+print("Dit geweldige dagje-uit met " + str(toegangsticketaantal) + " mensen in de Speelhal met " + str(totaalaantalminuten) + " minuten VR kost je maar " + str(totaalprijs) + " euro.")
